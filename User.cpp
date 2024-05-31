855e778e20ba998cda6cf8c7b704acd5ec2f2640
Ali-Madian <167026819+Ali-Madian@users.noreply.github.com>
2024-06-01 02:46:18 +0300

    Update User.cpp
    f

--- User.cpp
+++ User.cpp
@@ -12,7 +12,7 @@ User::User(string UserName, string Password, double balance, bool sus)
 	this->Password = Password;
 	this->balance = balance;
 	this->suspended = sus;
-	
+
 }
 
 User::User(string UserName, string Password, double balance)
@@ -28,97 +28,102 @@ void User::BalanceAfterTransaction(double newBalance)
 	this->balance = newBalance;
 }
 
-void User::editUsername(string new_name)
-{
-
-	/*string new_name;
-	cout << "Please Enter The New Username :";
-	cin >> new_name;
-
-	if (System::search_user(new_name))
-	{
-		cout << "The Username already exist :\n";
-		return editUsername();
-
-	}*/
-	string oldName = UserName;
-	//handling transaction history after edit name 
-	{
-		for (int i = 0; i < System::allTransactions.size(); i++) {
-			if (System::allTransactions[i].getSender() == oldName) {
-				System::allTransactions[i].SetSender(new_name);
-			}
-			else if (System::allTransactions[i].getReciever() == oldName)
-				System::allTransactions[i].reciever = new_name;
-		}
-		for (int i = 0; i < History.size(); i++) {
-			if (History[i].getSender() == oldName) {
-				User* R = System::getUser(History[i].getReciever());
-				History[i].SetSender(new_name);
-				for (int j = 0; j < R->History.size(); j++) {
-					if (R->History[j].getSender() == oldName)
-						R->History[j].sender = new_name;
-				}
-			}
-			else if (History[i].getReciever() == oldName) {
-				User* R = System::getUser(History[i].getSender());
-				History[i].reciever = new_name;
-				for (int j = 0; j < R->History.size(); j++) {
-					if (R->History[j].reciever == oldName)
-						R->History[j].reciever = new_name;
-				}
-
-			}
-		}
-	}
-	//handling pending transaction after edit name
-	{
-		for (auto& request : System::allPendingRequests) {
-			// Check if the sender's username matches the old username
-			if (request.sender == oldName) {
-				// Update the sender's username to the new username
-				request.sender = new_name;
-			}
-			// Check if the receiver's username matches the old username
-			if (request.reciever == oldName) {
-				// Update the receiver's username to the new username
-				request.reciever = new_name;
-
-				User* R = System::getUser(request.sender);
-				for(auto& request1:R->pendingRequests)
-				{
-					if(request1.reciever==oldName)
-					request1.reciever = new_name;
-				}
-				
-			}
-		}
-
-		for (auto& request : pendingRequests) {
-			// Check if the sender's username matches the old username
-			if (request.sender == oldName) {
-				// Update the sender's username to the new username
-				request.sender = new_name;
-			}
-			// Check if the receiver's username matches the old username
-			
-		}
-
-
-	}
-	UserName = new_name;
-	System::allUsers[new_name] = System::allUsers[oldName];
-	System::allUsers.erase(oldName);
-	cout << "the Username has been Changed Successfully \n";
-	
+void User::editUsername(string &new_name){
+//
+//	/*string new_name;
+//	cout << "Please Enter The New Username :";
+//	cin >> new_name;
+//
+//	if (System::search_user(new_name))
+//	{
+//		cout << "The Username already exist :\n";
+//		return editUsername();
+//
+//	}*/
+//	string oldName = UserName;
+//	//handling transaction history after edit name 
+//	{
+//		for (int i = 0; i < System::allTransactions.size(); i++) {
+//			if (System::allTransactions[i].getSender() == oldName) {
+//				System::allTransactions[i].SetSender(new_name);
+//			}
+//			else if (System::allTransactions[i].getReciever() == oldName)
+//				System::allTransactions[i].reciever = new_name;
+//		}
+//		for (int i = 0; i < History.size(); i++) {
+//			if (History[i].getSender() == oldName) {
+//				User* R = System::getUser(History[i].getReciever());
+//				History[i].SetSender(new_name);
+//				for (int j = 0; j < R->History.size(); j++) {
+//					if (R->History[j].getSender() == oldName)
+//						R->History[j].sender = new_name;
+//				}
+//			}
+//			else if (History[i].getReciever() == oldName) {
+//				User* R = System::getUser(History[i].getSender());
+//				History[i].reciever = new_name;
+//				for (int j = 0; j < R->History.size(); j++) {
+//					if (R->History[j].reciever == oldName)
+//						R->History[j].reciever = new_name;
+//				}
+//
+//			}
+//		}
+//	}
+//	//handling pending transaction after edit name
+//	{
+//		for (auto& request : System::allPendingRequests) {
+//			// Check if the sender's username matches the old username
+//			if (request.sender == oldName) {
+//				// Update the sender's username to the new username
+//				request.sender = new_name;
+//			}
+//			// Check if the receiver's username matches the old username
+//			if (request.reciever == oldName) {
+//				// Update the receiver's username to the new username
+//				request.reciever = new_name;
+//
+//				User* R = System::getUser(request.sender);
+//				for (auto& request1 : R->pendingRequests)
+//				{
+//					if (request1.reciever == oldName)
+//						request1.reciever = new_name;
+//				}
+//
+//			}
+//		}
+//
+//		for (auto& request : pendingRequests) {
+//			// Check if the sender's username matches the old username
+//			if (request.sender == oldName) {
+//				// Update the sender's username to the new username
+//				request.sender = new_name;
+//			}
+//			// Check if the receiver's username matches the old username
+//
+//		}
+//
+//
+//	}
+//	UserName=new_name;
+//
+//	User userrr = System::allUsers[oldName];
+//
+//	userrr.UserName = new_name;
+//	System::allUsers.erase(oldName);
+//
+//	System::allUsers[new_name] = userrr;
+//	cout << "the Username has been Changed Successfully \n";
+//	cout << balance;
+	Admin::edit_username(UserName, new_name);
 }
 
-void User::editPassword(string password)
+void User::editPassword(string &password)
 {
 	/*string new_pass;
 	cout << "Please Enter The New Password :";
 	cin >> new_pass;*/
-	
+
 	setpassword(System::sha256(password));
 	cout << "the Password has been Changed Successfully \n";
 }
@@ -166,7 +171,7 @@ void User::userData()
 	//ViewHistory();
 }
 
-void User::Send(string reciever,double amount)
+void User::Send(string reciever, double amount)
 {
 	//string reciever;
 	//double amount = -1;
@@ -269,11 +274,11 @@ void User::Send(string reciever,double amount)
 
 	//else
 	//{
-		transactions.amount = amount;
-		transactions.sender = System::loggedInUser->getUserName();
-		transactions.reciever = reciever;
-		transactions.Date = Transaction::getCurrentDateTime();
-		CheckOut(reciever);
+	transactions.amount = amount;
+	transactions.sender = System::loggedInUser->getUserName();
+	transactions.reciever = reciever;
+	transactions.Date = Transaction::getCurrentDateTime();
+	CheckOut(reciever);
 	//}
 
 
@@ -337,22 +342,22 @@ void User::CheckOut(string reciever)
 		{
 */
 
-			User* Reciever = System::getUser(reciever);
+	User* Reciever = System::getUser(reciever);
 
 
-			double SenderNewBalance = ViewCurrentBalance() - transactions.getAmount();
+	double SenderNewBalance = ViewCurrentBalance() - transactions.getAmount();
 
-			double RecieverNewBalance = Reciever->ViewCurrentBalance() + transactions.getAmount();
+	double RecieverNewBalance = Reciever->ViewCurrentBalance() + transactions.getAmount();
 
-			BalanceAfterTransaction(SenderNewBalance);
+	BalanceAfterTransaction(SenderNewBalance);
 
 
-			Reciever->BalanceAfterTransaction(RecieverNewBalance);
+	Reciever->BalanceAfterTransaction(RecieverNewBalance);
 
-			History.push_back(transactions);
+	History.push_back(transactions);
 
-			Reciever->History.push_back(transactions);
-			System::allTransactions.push_back(transactions);
+	Reciever->History.push_back(transactions);
+	System::allTransactions.push_back(transactions);
 
 
 	//	}
@@ -429,16 +434,16 @@ void User::RequestMoney(string& sender, double amount) {
 
 	//else
 	//{
-	
 
-		cout << "Request is sent successfully\n";
-		transactions.setAmount(amount);
-		transactions.SetSender(FindingUser->getUserName());
-		transactions.reciever = System::loggedInUser->getUserName();
-		System::allPendingRequests.push_back(transactions);
-		FindingUser->pendingRequests.push_back(transactions);
-		// display out pending request in system
-	//}
+
+	cout << "Request is sent successfully\n";
+	transactions.setAmount(amount);
+	transactions.SetSender(FindingUser->getUserName());
+	transactions.reciever = System::loggedInUser->getUserName();
+	System::allPendingRequests.push_back(transactions);
+	FindingUser->pendingRequests.push_back(transactions);
+	// display out pending request in system
+//}
 
 
 
@@ -515,54 +520,54 @@ string User::serializeToString() const
 
 User User::deserializeFromString(const string& str)
 {
-		istringstream iss(str);
-		string token;
+	istringstream iss(str);
+	string token;
 
-		// Read UserName
+	// Read UserName
+	getline(iss, token, '|');
+	string userName = token;
+	if (userName.substr(0, 8) != "History:")
+	{
+
+		// Read Password
 		getline(iss, token, '|');
-		string userName = token;
-		if (userName.substr(0, 8) != "History:")
-		{
+		string password = token;
 
-			// Read Password
-			getline(iss, token, '|');
-			string password = token;
+		bool sus = false;
 
-			bool sus = false;
+		// Read balance
+		getline(iss, token, '|');
+		double balance = stod(token);
 
-			// Read balance
-			getline(iss, token, '|');
-			double balance = stod(token);
-
-			getline(iss, token, '|');
-			if (token == "true" || token == "1" || token == "TRUE") {
-				sus = true;
-			}
+		getline(iss, token, '|');
+		if (token == "true" || token == "1" || token == "TRUE") {
+			sus = true;
+		}
 
 
 
-			User user(userName, password, balance, sus);
+		User user(userName, password, balance, sus);
 
-			// Read remaining lines for History
-			while (getline(iss, token)) {
-				if (!token.empty()) {
-					if (token.find("History:") == 0) {
-						string historyData = token.substr(8); // Extract the transaction part
-						if (historyData != "EMPTY") { // Skip empty transactions
-							Transaction historyTransaction = Transaction::deserializeFromString(historyData);
-							user.addTransactionToHistory(historyTransaction);
-						}
+		// Read remaining lines for History
+		while (getline(iss, token)) {
+			if (!token.empty()) {
+				if (token.find("History:") == 0) {
+					string historyData = token.substr(8); // Extract the transaction part
+					if (historyData != "EMPTY") { // Skip empty transactions
+						Transaction historyTransaction = Transaction::deserializeFromString(historyData);
+						user.addTransactionToHistory(historyTransaction);
 					}
 				}
 			}
-			return user;
-			
 		}
-		else {
-			User user("Not good user", "-500", 0, true);
-			return user;
-		}
-		
+		return user;
+
+	}
+	else {
+		User user("Not good user", "-500", 0, true);
+		return user;
+	}
+
 }
 
 
